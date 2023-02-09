f9223983ad2fc5e3555948700e9953656666b831
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-09 14:02:43 +0200

    well done

--- js/movieApp.js
+++ js/movieApp.js
@@ -1,12 +1,14 @@
 const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=db5c04dbd9637821020050cea594d5e0&page=1";
 
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
   
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=db5c04dbd9637821020050cea594d5e0&query=1"';
 
 
 const form = document.getElementById("movie-form");
 const search = document.getElementById("movie-search");
+const main = document.getElementById("movie-main");
 
 //Get initial movies
 getMovies(API_URL);
@@ -16,8 +18,41 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  showMovies(data.results)
 }
+
+function showMovies(movies) {
+  main.innerHTML = ''
+  movies.forEach(movie => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}" class="movie-img">
+      <div class="movie-info">
+        <h3 class="movie-title">${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3 class="overview-title">overview</h3>
+        ${overview}
+      </div>`
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red"
+  }
+}
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
